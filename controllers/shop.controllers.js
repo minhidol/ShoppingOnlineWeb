7fd37aa8aa697c3944b2957ad232c73fc05729bf
Nam <hoangnam12c12@gmail.com>
2021-08-13 23:42:23 +0700

    Nam rate product

--- controllers/shop.controllers.js
+++ controllers/shop.controllers.js
@@ -1,4 +1,5 @@
 const shopModel = require("../models/shop.models");
+const ratingModel = require("../models/rating.models");
 
 const shopInterface = async (req, res) => {
   try {
@@ -56,7 +57,89 @@ const openShop = async (req, res) => {
   }
 };
 
+const ratingInterface = async (req, res) => {
+  try {
+    const accountID = req.params.id;
+    let invoiceID = await ratingModel.getInvoiceID(accountID);
+
+    html = "<h5>Please rate your orders</h5>";
+    await Promise.all(
+      invoiceID.map(async (invoiceid) => {
+        let result = await ratingModel.showInvoiceDetail(
+          invoiceid["InvoiceID"]
+        );
+        await Promise.all(
+          result.map(async (item) => {
+            html += `<div class="bg-light mb-3 p-4">`;
+            html += `<div class="invoice-wrapper d-flex flex-column">`;
+            html += `<div class="my-3">`;
+            html += `<div class="order-content d-flex flex-row border-top p-2">`;
+            html += `<div class="order-info">`;
+            html += `<img src="/images/image.png" width="10%">`;
+            html += `<div class="ml-3 mt-3">`;
+            html += `<h5 class="text-left">${item.ProductName}</h5>`;
+            html += `<p>x${item.Qty}</p>`;
+            html += `</div>`;
+            html += `</div>`;
+            html += `<p><small>$</small>${item.Price}</p>`;
+            html += `</div>`;
+            html += `</div>`;
+            html += `<h4 class="text-right">Total:`;
+            html += `<span class="text-danger"><small>$</small>${item.Subtotal}</span>`;
+            html += `</h4>`;
+            html += `<form action="http://localhost:3000/profile/${accountID}/openRating" method="POST">`;
+            html += `<input type="hidden" name="accountIDComment" value=${accountID} />`;
+            html += `<input type="hidden" name="productIDComment" value=${item.ProductID} />`;
+            html += `<div class="form-group d-flex flex-row">`;
+            html += `<h5>Star rate: </h5>`;
+            html += `<select class="form-control" name="starRate" style="margin-left: 20px; margin-top: -10px; width: 7%">`;
+            html += `<option value="5">5</option>`;
+            html += `<option value="4">4</option>`;
+            html += `<option value="3">3</option>`;
+            html += `<option value="2">2</option>`;
+            html += `<option value="1">1</option>`;
+            html += `</select>`;
+            html += `</div>`;
+            html += `<div class="form-group d-flex flex-row" style="align-items: center; justify-content:space-between">`;
+            html += `<textarea name="comment" id="" cols="900" rows="2" style="margin-right: 10px; padding: 5px" ></textarea>`;
+            html += `<input class="btn btn-success form-control" type="submit" name="commentSubmit" value="Comment" style="margin-top: 15px;">`;
+            html += `</div>`;
+            html += `</form>`;
+            html += `</div>`;
+            html += `</div>`;
+
+            // console.log(item.InvoiceID);
+          })
+        );
+      })
+    );
+    res.send(html);
+  } catch (error) {
+    throw error;
+  }
+};
+
+const RateProduct = async (req, res) => {
+  try {
+    await ratingModel.RateProduct(
+      req.body.accountIDComment,
+      req.body.productIDComment,
+      req.body.starRate,
+      req.body.comment
+    );
+    res.send(
+      `<script type="text/javascript"> alert("Rating success");</script>`
+    );
+
+    // console.log(req.body);
+  } catch (error) {
+    throw error;
+  }
+};
+
 module.exports = {
   shopInterface,
   openShop,
+  ratingInterface,
+  RateProduct,
 };
--- models/rating.models.js
+++ models/rating.models.js
@@ -12,6 +12,50 @@ const getRating = async (body) => {
   } catch (error) {}
 };
 
+const getInvoiceID = async (accountID) => {
+  try {
+    let pool = await sql.connect(config);
+    let invoiceID = await pool
+      .request()
+      .query(
+        `SELECT InvoiceID FROM Invoices WHERE CustomerAccountID = ${accountID}`
+      );
+
+    return invoiceID.recordset;
+  } catch (error) {}
+};
+
+const showInvoiceDetail = async (invoiceID) => {
+  try {
+    let pool = await sql.connect(config);
+    let result = await pool
+      .request()
+      .input("revoicesID", sql.Int, invoiceID)
+      .execute("showInvoiceDetail");
+
+    return result.recordset;
+  } catch (error) {}
+};
+
+const RateProduct = async (accountID, productID, starRate, comment) => {
+  try {
+    let pool = await sql.connect(config);
+    let result = await pool
+      .request()
+      .input("customerAccountID", sql.Int, accountID)
+      .input("productID", sql.Int, productID)
+      .input("starRate", sql.Float, starRate)
+      .input("comment", sql.VarChar(200), comment)
+      .execute("RateProduct");
+    return result.recordset;
+  } catch (error) {
+    throw error;
+  }
+};
+
 module.exports = {
   getRating,
+  getInvoiceID,
+  showInvoiceDetail,
+  RateProduct,
 };
