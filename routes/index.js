80fc585e518679ac5895dd5cd24b91221662d63d
Nam <hoangnam12c12@gmail.com>
2021-08-14 17:11:18 +0700

    Order history

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,12 @@ var router = express.Router();
 const productRoutes = require("./product.route");
 const billRoutes = require("./bill.route");
 const profileRoutes = require("./profile.route");
+const ratingRoutes = require("./rating.route");
 
 router.use("/product", productRoutes);
 router.use("/bill", billRoutes);
 router.use("/profile", profileRoutes);
 router.use("/home", productRoutes);
+router.use("/rating", ratingRoutes);
 
 module.exports = router;
